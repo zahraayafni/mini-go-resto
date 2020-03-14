e13920a4a3ea2172b981114c18ee71ce1f9418cd
Joseph <joseph_salimin@hotmail.com>
2020-03-14 21:25:46 +0700

    Fix food scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_095803) do
+ActiveRecord::Schema.define(version: 2020_03_14_142317) do
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
