54e7c1a35fa858db41c8d9af6cef7e938c571202
Joseph <joseph_salimin@hotmail.com>
2020-03-14 21:11:54 +0700

    Finishing food model and its test

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -9,5 +9,30 @@ RSpec.describe Food, type: :model do
     it 'Valid food with complete variable' do
       expect(@food).to be_valid
     end
+
+    it 'Name should be present' do
+      @food.name = nil
+      expect(@food).not_to be_valid
+    end
+
+    it 'Name should be longer than 3' do
+      @food.name = 'ab'
+      expect(@food).not_to be_valid
+    end
+
+    it 'Description should be present' do
+      @food.description = nil
+      expect(@food).not_to be_valid
+    end
+
+    it 'Price should be present' do
+      @food.price = nil
+      expect(@food).not_to be_valid
+    end
+
+    it 'Price should be integer' do
+      @food.price = 'not integer'
+      expect(@food).not_to be_valid
+    end
   end
 end
